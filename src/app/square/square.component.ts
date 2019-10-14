b67942facb1019494af02c613bca4c4da5b9e814
Maicon Andraski <maiconm23@gmail.com>
2019-10-13 21:16:54 -0300

    Square component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
+    <button>
+      {{ value }}
+    </button>
   `,
   styles: []
 })
-export class SquareComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
 }
