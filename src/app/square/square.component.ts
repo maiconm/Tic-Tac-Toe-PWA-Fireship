2c7bc97b011b2adef4022c76f63711185b5a1fa5
Maicon Andraski <maiconm23@gmail.com>
2019-10-15 06:43:39 -0300

    Nebular buttons style

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,7 +3,26 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>
+    <button
+      nbButton
+      *ngIf="!value"
+    >
+      {{ value }}
+    </button>
+    <button
+      nbButton
+      hero
+      status="success"
+      *ngIf="value == 'X'"
+    >
+      {{ value }}
+    </button>
+    <button
+      nbButton
+      hero
+      status="info"
+      *ngIf="value == 'O'"
+    >
       {{ value }}
     </button>
   `,
